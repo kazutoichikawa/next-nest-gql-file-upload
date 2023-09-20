f29926b31c2caba4bf0a745d5988d0970fe22fdf
kazutoichikawa <74768712+kazutoichikawa@users.noreply.github.com>
2023-09-20 18:19:58 +0900

    不足モジュールadd

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,22 @@
 'use client';
 import UploadFileForm from '@/components/UploadFileForm';
-import { Container } from '@mui/material';
+import { Button, Container } from '@mui/material';
 
 
 export default function Home() {
+  const handleClickTestGQL = () => {
+
+  }
+
   return (
     <Container>
       <h1 className="text-3xl">
         ファイルのアップロードをテストしてください
       </h1>
       <UploadFileForm />
+
+      <h2>graphqlのテスト</h2>
+      <Button>テスト</Button>
     </Container>
   );
 }
