cb98636356949852b854e3e728748bfa493c0ff1
kazutoichikawa <74768712+kazutoichikawa@users.noreply.github.com>
2023-09-20 18:17:47 +0900

    cors設定

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,27 @@ async function bootstrap() {
     AppModule,
     new ExpressAdapter()
   );
+
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      const allowedOrigins = [
+        'http://localhost:3001',
+        'http://localhost:3000',
+        'https://studio.apollographql.com',
+      ];
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy error: ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.use(
     '/graphql',
     graphqlUploadExpress({ maxFileSize: 100000000, maxFiles: 10 })
